acfb197e388492d73a24115755a57a920c3577dd
Novianto <viant.no@gmail.com>
2014-12-09 14:17:31 +0700

    update CSS
    Baru bisa edit ini

--- UAS/includes/layouts/footer.php
+++ UAS/includes/layouts/footer.php
@@ -1,12 +1,14 @@
 		<div id="footer">
-			&copy; Copyright <?php echo date("Y"); ?> Kurnia
+			Copyright &copy; <?php echo date("Y"); ?> Kurnia
 		</div><!--#footer-->
+		<!--
 		<br/>
 		<?php
-			date_default_timezone_set('Asia/Jakarta');
-			$date = date("Y-m-d H:i:s");
-			echo $date;
+			//date_default_timezone_set('Asia/Jakarta');
+			//$date = date("Y-m-d H:i:s");
+			//echo $date;
 		?> 
+		-->
 	</body>
 </html>
 <?php
